11151eacc6a1810fb1e12e89b470d55f5d4d167d
snpjames <85856584+snpjames@users.noreply.github.com>
2021-08-03 20:22:58 +0530

    Update utils.py

--- VideoEncoder/helpers/utils.py
+++ VideoEncoder/helpers/utils.py
@@ -15,16 +15,16 @@ async def on_task_complete():
 
 async def add_task(event):
     try:
-        msg = await event.reply("🟡 Downloading video...🟡\n\n⚙️Engine: Telethon\n\n#downloading`")
+        msg = await event.reply("🟡 Downloading video...🟡\n\n⚙️Engine: Telethon\n\n#downloading")
         filepath = await event.download_media(download_dir)
-        await msg.edit(" 🟣 Encoding video... 🟣\n\n⚙️Engine: FFMPEG\n\n#encoding`")
+        await msg.edit(" 🟣 Encoding video... 🟣\n\n⚙️Engine: FFMPEG\n\n#encoding")
         new_file = encode(filepath)
         if new_file:
             await msg.edit("`🔵 Getting metadata...🔵`")
             duration = get_duration(new_file)
             thumb = get_thumbnail(new_file, download_dir, duration / 4)
             width, height = get_width_height(new_file)
-            await msg.edit(" ⚪ Uploading video...⚪\n\n⚙️ Engine: Pyrogram\n\n#uploading`")
+            await msg.edit(" ⚪ Uploading video...⚪\n\n⚙️ Engine: Pyrogram\n\n#uploading ")
             await event.client.send_file(
                 event.chat_id,
                 file=new_file,
@@ -36,12 +36,12 @@ async def add_task(event):
             )
             remove(new_file)
             remove(thumb)
-            await msg.edit(" 🟢 Video Encoded 🟢\n\n#encoded`")
+            await msg.edit(" 🟢 Video Encoded 🟢\n\n#encoded ")
         else:
             await msg.edit(
-                " 🔴 Something wents wrong while encoding your file.`"
+                " 🔴 Something wents wrong while encoding your file."
             )
             remove(filepath)
     except Exception as e:
-        await msg.edit(f"**🔴 ERROR 🔴**:\n\n`{e}\n\n#error`")
+        await msg.edit(f"**🔴 ERROR 🔴**:\n\n`{e}\n\n#error")
     await on_task_complete()
