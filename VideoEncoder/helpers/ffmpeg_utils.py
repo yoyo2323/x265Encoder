4da2e74d96748981380c58b82e893d4c7a74882a
snpjames <85856584+snpjames@users.noreply.github.com>
2021-08-03 17:55:04 +0530

    Update ffmpeg_utils.py

--- VideoEncoder/helpers/ffmpeg_utils.py
+++ VideoEncoder/helpers/ffmpeg_utils.py
@@ -1,109 +1,79 @@
-#  Copyright (C) 2021 The Authors
-
 import os
+import sys
+import json
 import time
 import ffmpeg
 from subprocess import call, check_output
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-import logging
 
-
-def get_codec(filepath, channel="v:0"):
-    output = check_output(
-        [
-            "ffprobe",
-            "-v",
-            "error",
-            "-select_streams",
-            channel,
-            "-show_entries",
-            "stream=codec_name,codec_tag_string",
-            "-of",
-            "default=nokey=1:noprint_wrappers=1",
-            filepath,
-        ]
-    )
-    return output.decode("utf-8").split()
-
+def get_codec(filepath, channel='v:0'):
+    output = check_output(['ffprobe', '-v', 'error', '-select_streams', channel,
+                            '-show_entries', 'stream=codec_name,codec_tag_string', '-of', 
+                            'default=nokey=1:noprint_wrappers=1', filepath])
+    return output.decode('utf-8').split()
 
 def encode(filepath):
-    basefilepath = os.path.splitext(filepath)[0]
-    output_filepath = basefilepath + " [HEVC]" + ".mp4"
-    assert output_filepath != filepath
+    basefilepath, extension = os.path.splitext(filepath)
+    output_filepath = basefilepath + '.[HEVC]' + '.mp4'
+    assert(output_filepath != filepath)
     if os.path.isfile(output_filepath):
-        logging.info('Skipping "{}": file already exists'.format(output_filepath))
+        print('Skipping "{}": File Already Exists 🐭'.format(output_filepath))
         return None
-    logging.info(filepath)
+    print(filepath)
     # Get the video channel codec
-    video_codec = get_codec(filepath, channel="v:0")
+    video_codec = get_codec(filepath, channel='v:0')
     if video_codec == []:
-        logging.info("Skipping: no video codec reported")
+        print('Skipping No Video Codec Reported 🐭')
         return None
     # Video transcode options
-    if video_codec[0] == "hevc":
-        if video_codec[1] == "hvc1":
-            logging.info("Skipping: already h265 / hvc1")
+    if video_codec[0] == 'hevc':
+        if video_codec[1] == 'hvc1':
+            print('Skipping :( Already In HEVC')
             return None
         else:
             # Copy stream to hvc1
-            video_opts = "-c:v copy -tag:v avc1"
+            video_opts = '-c:v copy -tag:v hvc1'
     else:
-        # video option to h265 / hvc1
-        codec_opts = "-c:v libx265"
-        profile_opts = "-profile:v high"
-        tag_opts =  "-tag:v avc1"
-        tune_opts = "-tune animation"
-        lvl_opts = "-level 3.1"
-        crf_opts = "-crf 24"
-        preset_opts = "-preset slow"
-        Resolution_opts = "-vf  scale=1280:720"
-        core_opts = "-threads 8"
-        audio_opts = "-c:a aac -b:a 128k"
-    call(
-        ["ffmpeg", "-i", filepath]
-        + codec_opts.split()
-        + profile_opts.split()
-        + crf_opts.split()
-        + tag_opts.split()
-        + preset_opts.split()
-        + lvl_opts.split()
-        + tune_opts.split()
-        + Resolution_opts.split()
-        + core_opts.split()
-        + audio_opts.split()
-        + [output_filepath]
-    )
+        # Transcode to h265 / hvc1
+        video_opts = '-c:v libx265 -crf 35 -tag:v hvc1 -preset slow -threads 8'
+    # Get the audio channel codec
+    audio_codec = get_codec(filepath, channel='a:0')
+    if audio_codec == []:
+        audio_opts = ''
+    elif audio_codec[0] == 'aac':
+        audio_opts = '-c:a copy'
+    else:
+        audio_opts = '-c:a aac -b:a 128k'
+    call(['ffmpeg', '-i', filepath] + video_opts.split() + audio_opts.split() + [output_filepath])
     os.remove(filepath)
     return output_filepath
 
-
 def get_thumbnail(in_filename, path, ttl):
     out_filename = os.path.join(path, str(time.time()) + ".jpg")
-    open(out_filename, "a").close()
+    open(out_filename, 'a').close()
     try:
         (
-            ffmpeg.input(in_filename, ss=ttl)
+            ffmpeg
+            .input(in_filename, ss=ttl)
             .output(out_filename, vframes=1)
             .overwrite_output()
             .run(capture_stdout=True, capture_stderr=True)
         )
         return out_filename
     except ffmpeg.Error as e:
-        return None
-
+      return None
 
 def get_duration(filepath):
     metadata = extractMetadata(createParser(filepath))
     if metadata.has("duration"):
-        return metadata.get("duration").seconds
+      return metadata.get('duration').seconds
     else:
-        return 0
-
+      return 0
 
 def get_width_height(filepath):
     metadata = extractMetadata(createParser(filepath))
     if metadata.has("width") and metadata.has("height"):
-        return metadata.get("width"), metadata.get("height")
+      return metadata.get("width"), metadata.get("height")
     else:
-        return 1280, 720
+      return 1280, 720
